6ae69aac2e8c984b3377401f93bce7a600e6fb99
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-01-06 11:55:06 +0100

    minor changes

--- snake-pygame.py
+++ snake-pygame.py
@@ -1,11 +1,17 @@
 import pygame
 import sys
+import random
 
 #pygame init 
 pygame.init()
 
 #window creation 
 window = pygame.display.set_mode((600,600))
+
+#display rate
+clock = pygame.time.Clock()
+
+
 
 #game loop 
 while True : 
@@ -14,6 +20,14 @@ while True :
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    # surfaces
+    window.fill(pygame.Color('green'))
+
+    window.blit(test_surface,(150,150))
 
     # displaying elements 
     pygame.display.update()
+
+    # display rate 
+    clock.tick(60)
