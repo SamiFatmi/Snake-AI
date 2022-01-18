64c1d08fc46650335f75d78aedbdead969d23876
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-01-18 18:05:32 +0100

    update possible directions for agents

--- snake_training_academy.py
+++ snake_training_academy.py
@@ -1,3 +1,4 @@
+from tracemalloc import Snapshot
 import pygame, sys, random
 from pygame.math import Vector2
 
@@ -49,23 +50,95 @@ class AI():
     def __init__(self,screen = 1) -> None:
         self.screen = screen
         self.score = 0
-        self.weights = [0,0,0,0,0,0,0]
+        self.weights = [-10,1,10,5]
         self.snake = SNAKE(screen)
         self.fruit = FRUIT(screen)
 
     def decide_direction(self):
-        while True :
-            direction = random.choice([Vector2(1,0),Vector2(-1,0),Vector2(0,1),Vector2(0,-1)])
-            if direction + self.snake.body[0] - self.snake.body[1] != Vector2(0,0):
-                self.snake.direction = direction
-                break
+        current_direction = self.snake.body[1] - self.snake.body[0]
+        directions = [Vector2(1,0),Vector2(-1,0),Vector2(0,1),Vector2(0,-1)]
+
+        available_directions = []
+        for direction in directions : 
+            if self.snake.body[0][0] + direction[0]<=n_cells-1 and self.snake.body[0][1] + direction[1]<=n_cells-1 and self.snake.body[0][0] + direction[0]>=0 and self.snake.body[0][1] + direction[1]>=0 and self.snake.body[0] + direction not in self.snake.body[1:]:
+                available_directions.append(direction)
+
+        directions_scores = []
+        if len(available_directions)==1:
+            self.snake.direction = available_directions[0]
+        else :
+            for direction in available_directions : 
+                #dangerous_move 
+                if self.snake.body[0][0]+direction[0]>n_cells-1 or self.snake.body[0][1]+direction[1]>n_cells-1 or self.snake.body[0][0]+direction[0]<0 or self.snake.body[0][1]+direction[1]<0 or self.snake.body[0]+direction in self.snake.body[2:] :
+                    dangerous_move = 1
+                else : 
+                    dangerous_move = 0 
+
+                #towards_fruit
+                if self.snake.body[0][0] == self.fruit.position[0] and direction[0]==0 : 
+                    if direction[1] == 1 and self.fruit.position[1] - self.snake.body[0][1] > 0:
+                        towards_fruit = 1
+                    elif direction[1] == -1 and self.fruit.position[1] - self.snake.body[0][1] < 0:
+                        towards_fruit = 1
+                    else :
+                        towards_fruit = 0
+                elif self.snake.body[0][1] == self.fruit.position[1] and direction[1] == 0  :
+                    if direction[0] == 1 and self.fruit.position[0] - self.snake.body[0][0] > 0:
+                        towards_fruit = 1
+                    elif direction[0] == -1 and self.fruit.position[0] - self.snake.body[0][0] < 0:
+                        towards_fruit = 1
+                    else :
+                        towards_fruit = 0
+                else :
+                    towards_fruit = 0 
+
+                #save_from_danger 
+                if self.snake.body[0] + current_direction in self.snake.body[1:] or self.snake.body[0][0]+current_direction[0]>n_cells-1 or self.snake.body[0][1]+current_direction[1]>n_cells-1 or self.snake.body[0][0]+current_direction[0]<0 or self.snake.body[0][1]+current_direction[1]<0 :
+                    if not (self.snake.body[0][0]+direction[0]>n_cells-1 or self.snake.body[0][1]+direction[1]>n_cells-1 or self.snake.body[0][0]+direction[0]<0 or self.snake.body[0][1]+direction[1]<0 or self.snake.body[0]+direction in self.snake.body[1:]) :
+                        save_from_danger = 1 
+                    else : 
+                        save_from_danger = 0 
+                else : 
+                    save_from_danger = 0
+
+                
+                #eat the fruit 
+                if self.snake.body[0] + direction == self.fruit.position : 
+                    eat_fruit = 1
+                else:
+                    eat_fruit = 0
+
+                score = dangerous_move*self.weights[0] + towards_fruit*self.weights[1] + save_from_danger*self.weights[2] + eat_fruit*self.weights[3]
+
+                directions_scores.append([score,direction])
+
+            if len(directions_scores) == 3 : 
+                if directions_scores[0][0] > directions_scores[1][0] :
+                    if directions_scores[0][0] > directions_scores[2][0] :
+                        self.snake.direction = directions_scores[0][1]
+                    else :
+                        self.snake.direction = directions_scores[2][1]
+                else : 
+                    if directions_scores[1][0] > directions_scores[2][0] :
+                        self.snake.direction = directions_scores[1][1]
+                    else : 
+                        self.snake.direction = directions_scores[2][1]
+            elif len(directions_scores) == 2 :
+                if directions_scores[0][0] > directions_scores[1][0] :
+                    self.snake.direction = directions_scores[0][1]
+                else : 
+                    self.snake.direction = directions_scores[1][1]
+            elif len(directions_scores)==1:
+                self.snake.direction == directions_scores[0][1]
+
+    
 
 
     def update(self):
         self.snake.move_snake()
         self.decide_direction()
-        self.check_death()
         self.check_collision()
+        self.check_death()
 
     def draw_elements(self):
         self.snake.draw_snake()
@@ -80,7 +153,7 @@ class AI():
                     break
 
     def check_death(self):
-        if self.snake.body[0] in self.snake.body[1:]:
+        if self.snake.body[0] in self.snake.body[2:]:
             self.game_over()
 
         if self.snake.body[0][0] < 0 or self.snake.body[0][0] > n_cells-1 or self.snake.body[0][1] < 0 or self.snake.body[0][1] > n_cells-1 :
@@ -89,6 +162,7 @@ class AI():
     def game_over(self):
         self.snake = SNAKE(self.screen)
         self.fruit = FRUIT(self.screen)
+        self.weights = [ random.random() for x in range(4)]
 
 
         
@@ -125,7 +199,7 @@ clock = pygame.time.Clock()
 # screen update 
 
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE,100)
+pygame.time.set_timer(SCREEN_UPDATE,50)
 
 
 #game loop 
